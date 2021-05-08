11a2e3c65ff2d61d3f9d3d45663009fa035e7489
Jason Raimondi <jason@raimondi.us>
2021-05-07 22:07:57 -0700

    refactor: extract to async sendEth method

--- src/main.ts
+++ src/main.ts
@@ -5,38 +5,42 @@ import type { TransactionConfig, TransactionReceipt } from "web3-core";
 
 const web3 = new Web3(Web3.givenProvider || process.env.WEB3_PROVIDER);
 
-(async () => {
+async function sendEth({ from, to, amount }: { from: string; to: string; amount: string }) {
+  const value = web3.utils.toWei(amount, "ether");
+  const currentBalance = await web3.eth.getBalance(to);
+  console.log({ value, currentBalance });
 
-  const amount = 5
-  const value = web3.utils.toWei(amount.toString(), 'ether')
-
-  console.log({ value, amount });
-
-  const [from, to] = await web3.eth.getAccounts();
-  console.log("balance: ", await web3.eth.getBalance(to));
   const tx: TransactionConfig = { from, to, value };
-  const gasEstimate = await web3.eth.estimateGas(tx);
   tx.gas = await web3.eth.estimateGas(tx);
 
-  console.log({ tx });
-
+  const privateKey = "9c0719c2af30bbdb997c666cfcf73a1255d7a44c5a0df68e7523221e44fd04d7"; // testing key from ganache
+  // const privateKey = "9c0719c2af30bbdb997c888cfcf73a1255d7a44c5a0df68e7523221e44fd04d7"; // not valid
+  const signedTransaction = await web3.eth.accounts.signTransaction(tx, privateKey);
+  if (typeof signedTransaction.rawTransaction !== "string") throw new Error("invalid signedTransaction");
   const receipt = await web3.eth
-    .sendTransaction({
-      ...tx,
-      gas: gasEstimate,
-    })
+    .sendSignedTransaction(signedTransaction.rawTransaction)
     .on("transactionHash", (txHash: string) => {
-      console.log({ txHash });
+      // console.log({ txHash });
     })
     .on("receipt", (receipt: TransactionReceipt) => {
-      console.log({ receipt });
+      // console.log({ receipt });
     })
     .on("confirmation", (confNumber: number, receipt: TransactionReceipt, latestBlockHash?: string) => {
-      console.log({ confNumber, receipt, latestBlockHash });
+      // console.log({ confNumber, receipt, latestBlockHash });
     })
     .on("error", (error: Error) => {
       console.log({ error });
     });
 
-  console.log("balance: ", await web3.eth.getBalance(to));
-})().finally(() => process.exit());
+  console.log({ tx });
+  console.log("receipt: ", receipt);
+  console.log("balance to: ", await web3.eth.getBalance(to));
+  console.log("balance from: ", await web3.eth.getBalance(from));
+}
+
+(async () => {
+  const [_, _1, to, from] = await web3.eth.getAccounts();
+  await sendEth({ to, from, amount: "0.1" });
+})()
+  .catch(console.log)
+  .finally(process.exit);
